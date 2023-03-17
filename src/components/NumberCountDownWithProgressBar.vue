0cf0f5f7c2611a332282f84d511bf42094ce500a
Harry <harry750110@gmail.com>
2023-03-17 16:14:53 +0800

    提升程式可讀性

--- src/components/NumberCountDownWithProgressBar.vue
+++ src/components/NumberCountDownWithProgressBar.vue
@@ -5,33 +5,31 @@
   在倒數時間剩下3秒時，如果需要撥放音效，它會播放音效。當倒數計時器達到0時，它會顯示一個結束提示和一個 "重新開始" 按鈕。
   當 "重新開始" 按鈕被點擊時，它會重新設定倒數時間並重新啟用進度條的過渡效果。
 -->
-
 <template>
   <div>
     <div class="countdown-wrapper">
       <div class="countdown">
         <!-- 分鐘數字 -->
         <div class="number-wrapper">
-          <div class="number">{{ Math.floor(countdown / 60) }}</div>
+          <div class="number">{{ minutes }}</div>
         </div>
         <!-- 冒號分隔符號 -->
         <div class="separator">:</div>
         <!-- 秒數十位數字 -->
         <div class="number-wrapper">
-          <div class="number">{{ Math.floor(countdown % 60 / 10) }}</div>
+          <div class="number">{{ tenSeconds }}</div>
         </div>
         <!-- 秒數個位數字 -->
         <div class="number-wrapper">
-          <div class="number">{{ Math.floor(countdown % 10) }}</div>
+          <div class="number">{{ seconds }}</div>
         </div>
 
         <!-- 倒數計時結束提示 -->
         <div v-if="countdown <= 0" class="countdown-finished">
           時間到了！
-          <button @click="rest" class="rest-button">重新開始</button> <!-- rest按鈕 -->
+          <button @click="reset" class="rest-button">重新開始</button>
         </div>
       </div>
-
     </div>
 
     <div class="countdown-wrapper-2">
@@ -42,13 +40,8 @@
         </div>
       </div>
     </div>
-
-
   </div>
-
-
 </template>
-
 <script>
   export default {
     props: {
@@ -58,7 +51,7 @@
         required: true,
         default: 60,
       },
-      // 是否要撥放音效 
+      // 是否要撥放音效
       sound: {
         type: Boolean,
         required: false,
@@ -69,44 +62,63 @@
       return {
         countdown: this.time, // 倒數的初始時間（秒）
         audio: null, // 音效檔案物件
+        timer: null, // 計時器物件
       };
     },
     computed: {
+      // 分鐘數
+      minutes() {
+        return Math.floor(this.countdown / 60);
+      },
+      // 秒數（十位數）
+      tenSeconds() {
+        return Math.floor(this.countdown % 60 / 10);
+      },
+      // 秒數（個位數）
+      seconds() {
+        return Math.floor(this.countdown % 10);
+      },
+      // 進度條寬度
       progressBarWidth() {
-        return ((this.countdown / this.time) * 100)
+        return ((this.countdown / this.time) * 100);
       }
     },
     mounted() {
-      this.timer = setInterval(this.tick, 1000); // 每秒呼叫tick方法
-      this.loadAudio(); // 載入音效
+      // 每秒呼叫tick方法
+      this.timer = setInterval(this.tick, 1000);
+      // 載入音效
+      this.audio = new Audio(require('@/assets/sounds/countdown.mp3'));
+      this.audio.load();
     },
     beforeUnmount() {
-      clearInterval(this.timer); // 在元件銷毀前停止計時器
+      // 在元件銷毀前停止計時器
+      clearInterval(this.timer);
     },
     methods: {
-      loadAudio() {
-        this.audio = new Audio(require('@/assets/sounds/countdown.mp3')); // 載入音效檔案
-        this.audio.load(); // 加載音效
-
-      },
+      // 撥放音效
       playAudio() {
-        const playPromise = this.audio.play(); // 播放音效
+        const playPromise = this.audio.play();
         if (playPromise !== undefined) {
           playPromise.catch(error => {
             console.error('Failed to play audio:', error);
           });
         }
       },
+      // tick方法
       tick() {
-        if (this.countdown > 0) { // 如果倒數時間大於0，則減去1秒
+        // 如果倒數時間大於0，則減去1秒
+        if (this.countdown > 0) {
           this.countdown--;
-          if (this.sound && this.countdown === 3) { // 如果需要撥放音效，且倒數時間剩下3秒時，撥放音效
+          // 如果需要撥放音效，且倒數時間剩下3秒時，撥放音效
+          if (this.sound && this.countdown === 3) {
             this.playAudio();
           }
         }
       },
-      rest() {
-        this.countdown = this.time; // 重新設定倒數時間為60秒
+      // 重新開始
+      reset() {
+        // 重新設定倒數時間為60秒
+        this.countdown = this.time;
         // 暫時取消進度條的過渡效果
         document.querySelector(".progress-bar").style.transition = "none";
         // 在下一個畫面更新周期重新啟用過渡效果
@@ -114,7 +126,7 @@
           document.querySelector(".progress-bar").style.transition = "width 0.5s";
         });
       }
-    },
+    }
   };
 </script>
 
@@ -140,6 +152,7 @@
     justify-content: center;
   }
 
+  /* 數字外框樣式 */
   .number-wrapper {
     display: flex;
     justify-content: center;
@@ -152,16 +165,17 @@
     font-weight: bold;
     color: #000;
     margin: 0 0.3rem;
-
     box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
   }
 
+  /* 冒號分隔符號樣式 */
   .separator {
     font-size: 80px;
     font-weight: bold;
     color: #000;
   }
 
+  /* 倒數計時結束提示樣式 */
   .countdown-finished {
     font-size: 24px;
     font-weight: bold;
@@ -171,6 +185,7 @@
     align-items: center;
   }
 
+  /* 重新開始按鈕樣式 */
   .rest-button {
     background-color: #4caf50;
     border: none;
@@ -190,6 +205,7 @@
     background-color: #3e8e41;
   }
 
+  /* 進度條外框樣式 */
   .progress-bar-wrap {
     width: 100%;
     height: 20px;
@@ -198,6 +214,7 @@
     margin: 1em 0;
   }
 
+  /* 進度條樣式 */
   .progress-bar {
     height: 100%;
     background-image: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);
